a9555d9813dd5a1fd0af7735a0417530b9d0073d
Phil Tomson <philtomson@gmail.com>
2018-07-25 17:43:09 -0700

    start of support for functions with multiple args

--- src/LUT.jl
+++ src/LUT.jl
@@ -3,27 +3,68 @@
 # convert functions into lookuptables
 # useful for hardware modeling
 ##########################################################
-mutable struct LuT{A,R,F}
+mutable struct LUT{A,R,F}
    f::F
    r::R
    lut::A
 end
 
-function LuT(fn::Function, r::StepRangeLen)
+function LUT(fn::Function, r::StepRangeLen)
    lut = [fn(x) for x in r ]
-   LuT(fn, r, lut)
+   LUT(fn, r, lut)
 end
 
-function (lut::LuT)(x)
+function (lut::LUT)(x)
    if(x < lut.r[1] ||  x > lut.r[end])
       error("out of range!")
    end
    lut.lut[round(Int, x/lut.r.step.hi) + 1]
 end
 
+ ## The beginning of multi-var support:
+ #this one would be for passing a function as opposed to a formula:
+function build_lut(fn::Function, vars)
+   compstr = "[ $(fn)("
+   args    = []
+   forstr  = " "
+   for var in vars
+      vname = var[1]
+      vrange= var[2]
+      push!(args, vname)
+      forstr = forstr*"for $vname in $vrange "
+   end
+   compstr = compstr * join(args, ",") * ") "* forstr * "]"
+   @show compstr
+   @eval $(Meta.parse(compstr))
+end
+
+function build_lut(fn::Expr, vars)
+    compstr = "[ $(fn)"
+    args    = []
+    forstr  = " "
+    for var in vars
+       vname = var[1]
+       vrange= var[2]
+       push!(args, vname)
+       forstr = forstr*"for $vname in $vrange "
+    end
+    compstr = compstr * forstr * "]"
+    @show compstr
+    @eval $(Meta.parse(compstr))
+ end
+
+
+
  #try with identity function
-identLut = LuT(x -> x, 0:0.5:10.0)
+identLut = LUT(x -> x, 0:0.5:10.0)
 println(identLut(2.6))
 
-sinlut = LuT(sin, 0.0:(pi/100):2.0*pi)
+sinlut = LUT(sin, 0.0:(pi/100):2.0*pi)
+
+exprlut = build_lut(:(2*x*y^2), [(:x, 0.0:0.1:10.0), (:y, 0.0:0.1:10.0)])
+
+fn(x,y)   = 2x*y^2
+fnlut   = build_lut(fn, [(:x, 0.0:0.1:10.0), (:y, 0.0:0.1:10.0)])
+
+
 
